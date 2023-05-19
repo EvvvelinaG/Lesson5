51421abc08413e81713188c64e35c079f5b9238a
evagi <eva.gilyazova2003@gmail.com>
2023-05-19 23:53:52 +0300

    accelerometer

--- accelerometer/src/main/java/ru/mirea/gilyazoivaer/accelerometer/MainActivity.java
+++ accelerometer/src/main/java/ru/mirea/gilyazoivaer/accelerometer/MainActivity.java
@@ -11,26 +11,35 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
     private TextView azimuthTextView;
     private TextView pitchTextView;
     private TextView rollTextView;
     private SensorManager sensorManager;
     private Sensor accelerometerSensor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SensorManager sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
         azimuthTextView = findViewById(R.id.textViewAzimuth);
         pitchTextView = findViewById(R.id.textViewPitch);
         rollTextView = findViewById(R.id.textViewRoll);
     }
+
     @Override
     protected void onPause() {
         super.onPause();
         sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
@@ -44,6 +53,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             rollTextView.setText("Roll: " + valueRoll);
         }
     }
+
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
